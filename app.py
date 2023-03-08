947f7c4971463103b148cf06a5b516d2518c8b73
Yadheedhya.i <79125868+Yadheedhya06@users.noreply.github.com>
2023-03-09 01:27:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def inference(model_inputs:dict) -> dict:
         return {'message': "No prompt provided"}
     
     # Run the model
-    result = model(prompt, min_length=20, max_length=25)
+    result = model(prompt)
 
     # Return the results as a dictionary
     return result
